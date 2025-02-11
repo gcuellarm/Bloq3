51230d7831312539edc6ee7bcac1ddbf0063da3c
gcuellarm <gcuellarmerida@gmail.com>
2025-02-11 11:42:54 +0100

    Update RealCrypto.sol

--- Token/RealCrypto.sol
+++ Token/RealCrypto.sol
@@ -14,10 +14,7 @@ contract RealCrypto is ERC20, AccessControl, Pausable {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
 
-/*    constructor(address minter) public ERC20("MyToken", "TKN");
-        _setupRole(MINTER_ROLE, minter); //Grant the minter role to a specified account
 
-    }*/
     constructor(address minter_, address burner_, address pauser_) ERC20("MyToken", "TKN") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender); // The creator is the admin
         _grantRole(MINTER_ROLE, minter_); //Grant the minter role to a specified account
@@ -25,14 +22,22 @@ contract RealCrypto is ERC20, AccessControl, Pausable {
         _grantRole(PAUSER_ROLE, pauser_); //Grant the pauser (and unpause) role to a specified account, only to the owner
     }
 
-   
+//modifiers
+    //This modifier will check if the supply is higher than the amount to burn, if not, the function will not be executed
+    modifier burnPrevent(uint amount_, address account_){
+        if(amount_ > balanceOf(account_)) revert();
+        _;
+    }
+
+//functions   
 
     function mint(address to_ , uint amount_) public  onlyRole(MINTER_ROLE){
+        require(hasRole(MINTER_ROLE, msg.sender) || hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not a minter");
         _mint(to_, amount_);
     }
 
-    function burn(uint amount_) public onlyRole(BURNER_ROLE) {
-        _burn(msg.sender, amount_);
+    function burn(address to_, uint amount_) public burnPrevent(amount_, to_) onlyRole(BURNER_ROLE) {
+        _burn(to_, amount_);
     }
 
     function pause() public onlyRole(PAUSER_ROLE){
@@ -43,8 +48,12 @@ contract RealCrypto is ERC20, AccessControl, Pausable {
         _unpause();
     }
 
-    function balance(address account_) public view onlyRole(DEFAULT_ADMIN_ROLE) returns(uint){
+    function balance(address account_) public view onlyRole(DEFAULT_ADMIN_ROLE) returns(uint256){
         return balanceOf(account_);
+    }
+
+    function approveAmount(address to_, uint amount_) public onlyRole(DEFAULT_ADMIN_ROLE) returns(bool){
+        return approve(to_, amount_);
     }
 
 }
